93972e8cff5f5580c602471fc4c8c2d290c51fc0
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2020-05-16 23:04:02 +0300

    Add read trials into the project

---
+++ Avro-read.py
@@ -0,0 +1,12 @@
+import avro.schema
+import json
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+reader = DataFileReader(open("users.avro", "rb"), DatumReader())
+schema = reader.datum_reader.writer_schema
+print(schema)
+dict = json.loads(reader.meta.get('avro.schema').decode('utf-8'))
+for user in reader:
+    print(user)
+reader.close()
